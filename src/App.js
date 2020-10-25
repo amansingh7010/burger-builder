17a0a4f850481ed3d2b529c2902e8500a5c95464
Amanjot Singh <amansinghs7010@gmail.com>
2020-10-25 21:11:38 +0530

    integrate prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    );
+    return <div></div>
   }
 }
 
-export default App;
+export default App
