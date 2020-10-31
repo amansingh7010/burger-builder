7dc4d307da9d2fbe4cd67b857b8f48d9075f70ef
Amanjot Singh <amansinghs7010@gmail.com>
2020-10-31 12:12:16 +0530

    added total price in order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,6 +93,7 @@ class BurgerBuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchasCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
           />
