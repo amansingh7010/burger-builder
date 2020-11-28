2132bb3f00e83acb646d8220eccef9f3a93ab79a
Amanjot Singh <amansinghs7010@gmail.com>
2020-11-28 20:54:47 +0530

    connect orders page to redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,6 +3,7 @@ import * as actionTypes from '../actions/actionTypes'
 const initialState = {
   orders: [],
   loading: false,
+  purchased: false,
 }
 
 const reducer = (state = initialState, action) => {
@@ -20,9 +21,31 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
+        purchased: true,
         orders: state.orders.concat(newOrder),
       }
     case actionTypes.PURCHASE_BURGER_FAILED:
+      return {
+        ...state,
+        loading: false,
+      }
+    case actionTypes.PURCHASE_INIT:
+      return {
+        ...state,
+        purchased: false,
+      }
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true,
+      }
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        orders: action.orders,
+      }
+    case actionTypes.FETCH_ORDERS_FAILED:
       return {
         ...state,
         loading: false,
